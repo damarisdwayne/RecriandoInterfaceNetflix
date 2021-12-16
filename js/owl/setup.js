d590db6ecf4be1d44aede70e76459a60192d1cae
Dâmaris Dwayne <damarisdwayne@outlook.com>
2021-12-16 10:43:31 -0300

    Finalizada interface responsiva NETFLIX

---
+++ js/owl/setup.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
